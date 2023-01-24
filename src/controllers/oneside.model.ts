ee0b0dd1de45f0eb2361a9d848c1e6cc1b17cb66
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-01-24 11:03:14 +0100

    Új állományok feltöltése

---
+++ src/controllers/oneside.model.ts
@@ -0,0 +1,25 @@
+// https://mongoosejs.com/docs/validation.html#built-in-validators
+
+import { Schema, model } from "mongoose";
+
+const onesideSchema = new Schema(
+    {
+        _id: Number,
+        name: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+        },
+    },
+    // Virtuals are not included in string version of the model instances by default.
+    // To include them, set the virtuals option to true on schema’s toObject and toJSON options.
+    // { versionKey: false, id: false, toJSON: { virtuals: true }, toObject: { virtuals: true } },
+    { versionKey: false },
+);
+
+const onesideModel = model("oneside", onesideSchema, "TáblaNeve1");
+
+export default onesideModel;
