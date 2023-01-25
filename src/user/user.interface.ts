f3142629739b9b32b5834f5dd06e4d6495bb234f
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-01-25 11:23:50 +0100

    Mégis vissza
    Hibát okozott a törlés

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -3,6 +3,14 @@ export default interface IUser {
     _id?: Types.ObjectId | string;
     name: string;
     email: string;
+    email_verified: boolean;
+    auto_login: boolean;
+    picture: string;
     password: string;
     roles: string[];
+    address?: {
+        street: string;
+        city: string;
+        country: string;
+    };
 }
