8ef21f340c12973bbd5dfc7497d44f5193fc5bd5
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-01-25 10:21:30 +0100

    Régi fájlok törlése

--- src/server.ts
+++
@@ -1,7 +0,0 @@
-import App from "./app";
-// remove onesideController or nsideController, if you don't use it:
-import nsideController from "./controllers/nside.controller";
-import onesideController from "./controllers/oneside.controller";
-const app = new App([new nsideController(), new onesideController()]);
-
-app.listen();
