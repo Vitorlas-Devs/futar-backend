a37324ee1fc9f80f789fc2f3b10fa2f6ea54adcd
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-02-17 14:16:07 +0100

    Validator

--- src/díj/díj.model.ts
+++ src/díj/díj.model.ts
@@ -5,7 +5,15 @@ const díjSchema = new Schema<IDíj>(
     {
         _id: Number,
         minKm: { type: Number, required: true },
-        maxKm: { type: Number, required: true },
+        maxKm: {
+            type: Number,
+            required: true,
+            validate: {
+                validator: function (Rule: any) {
+                    return Rule.required().max(Rule.valueOfField("minKm"));
+                },
+            },
+        },
         összeg: { type: Number, required: true },
     },
     { versionKey: false },
