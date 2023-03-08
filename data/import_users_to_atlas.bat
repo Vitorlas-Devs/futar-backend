f0357a89ee38025c5f56009e8f81b781d321a87f
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-03-08 12:55:31 +0100

    User.json és .env nagyon menő

--- data/import_users_to_atlas.bat
+++ data/import_users_to_atlas.bat
@@ -1,2 +1,3 @@
-"c:\Program Files\MongoDB\Server\5.0\bin\mongoimport.exe" --uri="mongodb+srv://m001-student:m001-student@sandbox.ihhfgcz.mongodb.net/FutarDB" --collection=users --drop --file=users.json --jsonArray
+chcp 65001
+"c:\Program Files\MongoDB\Server\6.0\bin\mongoimport.exe" --uri="mongodb+srv://m001-student:m001-student@sandbox.ihhfgcz.mongodb.net/FutarDB" --collection=users --drop --file=user.json --jsonArray
 pause
