e93b87a69b3decbfd6ae242293e331b868a0cde0
Csepi Szilveszter <csepi.szilveszter@gmail.com>
2023-03-08 12:59:39 +0100

    Mongo 6.0 frissítés

--- data/export_users_from_atlas.bat
+++ data/export_users_from_atlas.bat
@@ -1,3 +1,3 @@
 chcp 65001
-"c:\Program Files\MongoDB\Server\5.0\bin\mongoexport.exe" --uri="mongodb+srv://m001-student:m001-student@sandbox.ihhfgcz.mongodb.net/FutarDB" --collection=users --out=users_new.json --jsonArray
+"c:\Program Files\MongoDB\Server\6.0\bin\mongoexport.exe" --uri="mongodb+srv://m001-student:m001-student@sandbox.ihhfgcz.mongodb.net/FutarDB" --collection=users --out=users_new.json --jsonArray
 pause
